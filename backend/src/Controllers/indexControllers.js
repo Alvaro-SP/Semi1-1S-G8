234c9d374eba8ef156e066fb379256b9798a92c1
Doned-CMD <bryanmendez101@gmail.com>
2023-05-02 03:08:24 -0600

    [Add] Funciones de chatbot
    se agrego 3 funciones de chatbot y la conexion al chatbot

--- backend/src/Controllers/indexControllers.js
+++ backend/src/Controllers/indexControllers.js
@@ -16,6 +16,13 @@ const jwt = require('jsonwebtoken');
 var dotenv = require("dotenv");
 dotenv.config();
 
+const lexruntime = new AWS.LexRuntimeV2({
+    region: 'us-east-1', // La región donde se creo el bot
+    accessKeyId: 'AKIARHSIZASZYK6DQ5EY',
+    secretAccessKey: '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'
+});
+
+
 const s3 = new AWS.S3({
     accessKeyId: 'AKIA3YXREBXTHSMUIZHY',
     secretAccessKey: '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',
@@ -153,7 +160,7 @@ exports.login = async (req, res) => {
                     c.end()
                     return res.jsonp({ Res: false })
                 }
-                c.query(`SELECT foto FROM usuarios WHERE correo='${Correo}';`, async function (err, result, field) {
+                c.query(`SELECT foto, id FROM usuarios WHERE correo='${Correo}';`, async function (err, result, field) {
                     console.log("result")
                     console.log(result)
                     if (err || result.length != 1) {
@@ -189,7 +196,7 @@ exports.login = async (req, res) => {
                             c.end()
                             return res.jsonp({ Res: false })
                         }
-                        const token = jwt.sign({ Correo }, 'clave-secreta', { expiresIn: '1h' })
+                        const token = jwt.sign({ Correo, id:result[0].id }, 'clave-secreta', { expiresIn: '1h' })
                         c.end()
                         return res.jsonp({ Res: true, token: token })
                     })
@@ -216,9 +223,12 @@ exports.login = async (req, res) => {
 
             cognitoUser.authenticateUser(authenticationDetails, {
                 onSuccess: (result) => {
-                    const token = jwt.sign({ Correo }, 'clave-secreta', { expiresIn: '1h' })
-                    c.end()
-                    return res.jsonp({ Res: true, token: token })
+                    c.query(`SELECT id FROM usuarios WHERE correo='${Correo}';`, function (err, resultquery, field) {
+
+                        const token = jwt.sign({ Correo, id: resultquery[0].id }, 'clave-secreta', { expiresIn: '1h' })
+                        c.end()
+                        return res.jsonp({ Res: true, token: token })
+                    })
                 },
                 onFailure: (err) => {
                     console.log("on failure")
@@ -661,7 +671,7 @@ exports.EditarUsuario = async (req, res) => {
 }
 
 exports.addfriend = async (req, res) => {
-    const data = req.body;
+    const data = req.params;
     let correo_usr = "";
     let correo_amigo_usr = "";
 
@@ -920,4 +930,365 @@ exports.updatestateFriend = async (req, res) => {
         console.log(e)
         res.jsonp({ Res: false })
     }
+}
+
+var dict ={};
+
+exports.sendmessage = async (req, res) => {
+    const data = req.body
+    // const data = req.params
+    // let message = ""
+    // let id = ""
+    
+    // jwt.verify(data, 'clave-secreta', (err, decodedToken) => {
+    //     if (err) { //Verificando que no haya errores
+    //         console.log("hubo un error en la decodificacion")
+    //         return res.jsonp({ Res: false })
+    //     }
+    //     // Obtener información del usuario del payload del token
+    //     message = decodedToken.message;
+    //     id = decodedToken.id;
+    // });
+
+    let message = data.message
+    let id = data.id
+    
+    //console.log(id)
+    if(dict[id] == undefined){
+        dict[id] = []
+    }
+    
+    try {
+        //console.log("-------------")
+        //console.log(postchatbot(message))
+        //console.log(dict[id][dict[id].length-1])
+        if(dict[id][dict[id].length-1] != undefined){
+            if(dict[id][dict[id].length-1].message == "Para finalizar, escribe la contraseña para loggearte en la cuenta."){
+                console.log("--------------------entro-------------------")
+                console.log(message)
+                console.log(message.length)
+                if(message.length < 8){
+                    dict[id].push({
+                        message: message,
+                        bot: false
+                    })
+                    dict[id].push({
+                        message: "La contraseña debe tener al menos 8 caracteres",
+                        bot: true
+                    })
+                    dict[id].push({
+                        message: "Para finalizar, escribe la contraseña para loggearte en la cuenta.",
+                        bot: true
+                    })
+                    return res.jsonp(dict[id])
+                }
+            }
+        }
+
+        postchatbot(message, '12'+id).then( async (answer) => {
+            dict[id].push({
+                message: message,
+                bot: false
+                })
+
+            dict[id].push({
+                message: answer.messages[0].content,
+                bot: true
+            })
+            
+            console.log(answer.messages[0].content)
+            //console.log(answer.sessionState.intent.name)
+
+            if(answer.sessionState.intent.name == "agregarAmigo"){
+                console.log("agregarAmigo")
+                if (answer.messages[0].content == "OK, se ha enviado una solicitud de amistad con exito al amigo"){
+                    let correo1 = ""
+                    let correo2 = ""
+                    for (let i = 0; i < dict[id].length; i++) {
+                        const element = dict[id][i];
+
+                        if(element.message == "Hola, seleccionaste la funcion agregar amigo \\n para agregar un Amigo me debes brindar tu correo electronico"){                      
+                            console.log("correo1")
+                            console.log(dict[id][i+1])
+                            correo1 = dict[id][i+1].message
+                        }else if (element.message == "a continuacion dame el correo de la persona que deseas agregar"){                      
+                            console.log("correo2")
+                            console.log(dict[id][i+1])
+                            correo2 = dict[id][i+1].message
+                        }
+                    }
+                    const respuesta = await agregarAmigo(correo1, correo2)
+                }
+            }else if(answer.sessionState.intent.name == "crearUsuario"){
+                console.log("crearUsuario")
+                if (answer.messages[0].content == "Si hay algo mas en que pueda ayudarte, estoy para servirte :)"){
+                    let nombre = "";
+                    let DPI = "";
+                    let correo = "";
+                    let password = "";
+                    for (let i = 0; i < dict[id].length; i++) {
+                        const element = dict[id][i];
+
+                        if(element.message == "Entiendo funcionalidad para crear un usuario iniciada.                       Para empezar podria decirme que su correo? "){                      
+                            console.log("correo")
+                            console.log(dict[id][i+1])
+                            correo = dict[id][i+1].message
+                        }else if(element.message == "Todo correcto, ahora porfavor escribir su nombre:"){
+                            console.log("nombre")
+                            console.log(dict[id][i+1])
+                            nombre = dict[id][i+1].message
+                        }else if(element.message == "A continuacion, podrias darme tu numero de DPI? "){
+                            console.log("DPI")
+                            console.log(dict[id][i+1])
+                            DPI = dict[id][i+1].message
+                        }else if(element.message == "Para finalizar, escribe la contraseña para loggearte en la cuenta."){
+                            console.log("password")
+                            console.log(dict[id][i+1])
+                            password = dict[id][i+1].message
+                        }
+                    }
+                    const registroterminado = await registrosinFoto(nombre, correo, DPI, password)
+                }               
+            }else if(answer.sessionState.intent.name == "traduccion"){
+                console.log("traduccion")
+                if (answer.messages[0].content == "Su texto traducido es el siguiente: "){
+                    let texto = ""
+                    for (let i = 0; i < dict[id].length; i++) {
+                        const element = dict[id][i];
+
+                        if(element.message == "Funcion de traducir detectada, a continuacion ingresa el texto que deseas traducir:"){                      
+                            console.log("texto")
+                            console.log(dict[id][i+1])
+                            texto = dict[id][i+1].message
+                        }
+                    }
+                    const respuesta = await traducirfunc(texto)
+                        
+                    dict[id].push({
+                        message: respuesta.descripcion,
+                        bot: true
+                    })
+                      
+                }
+            }
+
+            res.jsonp(dict[id])
+        },
+        (error) => {
+            console.log(error)
+            res.jsonp({ Res: false })
+        })
+    } catch (error) {
+        console.log(error)
+        res.jsonp({ Res: false })
+    }
+}
+
+const postchatbot = async (message, id) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const params = {
+                botAliasId: 'BRINKTKLPE',
+                localeId: 'es_419',
+                text: message,
+                botId: 'MAQQSYQLV7',
+                sessionId: id,
+            };
+
+            lexruntime.recognizeText(params, (err, data) => {
+                if (err) {
+                    console.log(err, err.stack);
+                    reject(err);
+                } else {
+                    //console.log(data.messages[0].content);
+                    //message(data.messages[0].content)
+                    resolve(data);
+                }
+            });
+
+            //return { Res: false };
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    })
+}
+
+const registrosinFoto = async (Nombre, Correo, Dpi, Password) => {
+    return new Promise((resolve, reject) => {
+        try {
+            let c = mysql.createConnection(config)
+            c.connect(function (err) {
+                if (err) {
+                    console.log(err)
+                    c.end()
+                    reject({ Res: false })
+                }
+                c.query(`SELECT * FROM usuarios WHERE correo='${Correo}'`, async function (err, result, field) {
+                    if (err || result.length >= 1) {
+                        console.log(err)
+                        console.log(result)
+                        c.end()
+                        reject({ Res: false })
+                    }
+
+                    // subiendo la foto al bucket
+                    const nameFoto = `Fotos_Perfil/${Correo}.jpg`
+
+                    // insertando informacion a cognito
+                    const attributeList = []
+
+                    const dataName = {
+                        Name: 'name',
+                        Value: Nombre
+                    }
+                    attributeList.push(new CognitoUserAttribute(dataName))
+
+                    const dataEmail = {
+                        Name: 'email',
+                        Value: Correo
+                    }
+                    attributeList.push(new CognitoUserAttribute(dataEmail))
+
+                    const dataDpi = {
+                        Name: 'custom:dpi',
+                        Value: Dpi
+                    }
+                    attributeList.push(new CognitoUserAttribute(dataDpi))
+
+                    const username = Correo
+
+                    cognito.signUp(
+                        username,
+                        md5(Password),
+                        attributeList,
+                        null,
+                        (err, data) => {
+                            if (err) {
+                                console.log(err)
+                                c.end()
+                                reject({ Res: false })
+                            }
+
+                        }
+                    )
+
+                    // insertando informacion a la base de datos
+                    c.query(`INSERT INTO usuarios (nombre_completo, dpi, foto, password, correo)
+                    VALUES('${Nombre}', '${Dpi}', 'https://semi1proyecto-g8.s3.amazonaws.com/${nameFoto}',
+                    '${md5(Password)}','${Correo}');`, function (err, result, field) {
+                        if (err) {
+                            console.log(err)
+                            c.end()
+                            reject({ Res: false })
+                        }
+                        c.end()
+                        resolve({ Res: true })
+                    })
+                })
+
+            })
+        } catch (e) {
+            console.log("e")
+            console.log(e)
+            reject({ Res: false })
+        }
+    })
+}
+
+const traducirfunc = async (descripcion ) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const params2 = {
+                SourceLanguageCode: 'es',
+                TargetLanguageCode: 'en',
+                Text: descripcion
+            }
+            translate.translateText(params2, (err2, data2) => {
+                cadena = ""
+                if (err2) {
+                    cadena += `${descripcion}`;
+                } else {
+                    cadena += `${data2.TranslatedText}`;
+                }
+                //console.log(data2)
+                resolve({ Res: true, descripcion: cadena })
+            })
+        } catch (e) {
+            console.log("e")
+            console.log(e)
+            reject({ Res: false })
+        }
+    })
+}
+
+const agregarAmigo = async (correo_usr, correo_amigo_usr) => {
+    return new Promise((resolve, reject) => {
+        try {
+            let c = mysql.createConnection(config)
+            c.connect(function (err) {
+                if (err) {
+                    console.log(err)
+                    c.end()
+                    reject({ Res: false })
+                }
+                c.query(`SELECT * from usuarios WHERE correo ='${correo_usr}' OR correo ='${correo_amigo_usr}';`, async function (err, result, field) {
+                
+                    if (err) {
+                        console.log(err)
+                        c.end()
+                        reject({ Res: false })
+                    }
+
+                    console.log(result)
+
+                    if (result.length != 2) {
+                        console.log("no existe alguno de los usuarios")
+                        c.end()
+                        reject({ Res: false })
+                    }else{
+                        let id_usr;
+                        let id_amigo_usr;
+                        for (let i = 0; i < result.length; i++) {
+                            const element = result[i];
+                            if(element.correo == correo_usr){
+                                id_usr = element.id;
+                            }
+                            if(element.correo == correo_amigo_usr){
+                                id_amigo_usr = element.id;
+                            }
+                        }
+                    
+                        c.query(`SELECT * FROM amigos WHERE (id = ${id_usr} AND usuarios_id = ${id_amigo_usr}) OR (id = ${id_amigo_usr} AND usuarios_id =  ${id_usr});`, async function (err, result, field) {
+                            if (err) {
+                                console.log(err)
+                                c.end()
+                                reject({ Res: false })
+                            }
+                            if (result.length != 0) {
+                                console.log("ya son amigos")
+                                c.end()
+                                reject({ Res: false })
+                            }else{
+                                console.log(result)
+                                c.query(`INSERT INTO amigos(id, state, usuarios_id) VALUES('${id_usr}',0,'${id_amigo_usr}');`, async function (err, result, field) {
+                                    if (err) {
+                                        console.log(err)
+                                        c.end()
+                                        reject({ Res: false })
+                                    }
+                    
+                                    resolve({ Res: true })
+                                })
+                            }
+                        })
+                    }
+                }) 
+            })
+        } catch (e) {
+            console.log("e")
+            console.log(e)
+            reject({ Res: false })
+        }
+    })
 }
